3dd46122dc086bfa89ed82d7bf310ccdad01bbf2
Willina <wsilvadev@gmail.com>
2020-12-29 11:09:10 -0300

    fix(layouts): chage main layout

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,107 +1,52 @@
 <template>
   <q-layout view="lHh Lpr lFf">
-    <q-header elevated>
-      <q-toolbar>
-        <q-btn
-          flat
-          dense
-          round
-          icon="menu"
-          aria-label="Menu"
-          @click="leftDrawerOpen = !leftDrawerOpen"
-        />
-
-        <q-toolbar-title>
-          Quasar App
+    <q-header
+    class="bg-white text-grey-10"
+     bordered>
+      <q-toolbar class="constrain" >  
+        <q-btn to="camera" class="large-screen-only q-mr-sm" icon="eva-camera-outline" size="18px" flat round dense/>
+        <q-separator spaced vertical class="large-screen-only"/>
+        <q-toolbar-title class="text-grand-hotel text-bold">
+          QuasarInsta
         </q-toolbar-title>
+        <q-btn to="/" class="large-screen-only" icon="eva-home-outline" size="18px" flat round dense/>
 
-        <div>Quasar v{{ $q.version }}</div>
-      </q-toolbar>
+        </q-toolbar>
     </q-header>
+<q-footer class="bg-white small-screen-only" bordered >
+    <q-tabs  class="text-grey-10" active-color="primary" indicator-color="transparent">
+    <q-route-tab to="/" icon="eva-home-outline"/>    
+    <q-route-tab to="camera" icon="eva-camera-outline"/>
+    </q-tabs>
+</q-footer>
 
-    <q-drawer
-      v-model="leftDrawerOpen"
-      show-if-above
-      bordered
-      content-class="bg-grey-1"
-    >
-      <q-list>
-        <q-item-label
-          header
-          class="text-grey-8"
-        >
-          Essential Links
-        </q-item-label>
-        <EssentialLink
-          v-for="link in essentialLinks"
-          :key="link.title"
-          v-bind="link"
-        />
-      </q-list>
-    </q-drawer>
-
-    <q-page-container>
+    <q-page-container class="bg-grey-1">
       <router-view />
     </q-page-container>
   </q-layout>
 </template>
 
 <script>
-import EssentialLink from 'components/EssentialLink.vue'
-
-const linksData = [
-  {
-    title: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'school',
-    link: 'https://quasar.dev'
-  },
-  {
-    title: 'Github',
-    caption: 'github.com/quasarframework',
-    icon: 'code',
-    link: 'https://github.com/quasarframework'
-  },
-  {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev'
-  },
-  {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev'
-  },
-  {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev'
-  },
-  {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev'
-  },
-  {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev'
-  }
-];
-
 export default {
   name: 'MainLayout',
-  components: { EssentialLink },
   data () {
     return {
-      leftDrawerOpen: false,
-      essentialLinks: linksData
+     
     }
   }
 }
 </script>
+
+<style lang="sass" >
+.q-toolbar
+  @media (min-width: $breakpoint-xs-max)
+    height: 57px
+  .q-toolbar_title
+  font-size: 30px
+  @media (max-width: $breakpoint-xs-max)
+    text-align: center
+.q-footer
+  .q-tab_icon 
+    font-size: 30px
+
+</style>
